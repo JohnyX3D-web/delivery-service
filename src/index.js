f7bd88150b3775fe77443e4fabf95b6e471e40b7
Jonyx3d <dinisan2112@gmail.com>
2022-07-29 20:25:08 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Choices from "choices.js";
+//import "choices.js/public/assets/styles/choices.min.css";
+
+const select = document.querySelector(".selec");
+
+const choices = new Choices(select);
+
+console.log(choices);
+
+// import { Modal } from "bootstrap";
+
+// console.log(Modal);
