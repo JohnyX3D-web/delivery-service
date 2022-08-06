ebf2ced07d647048c2f9a896ef92ebb29bc61d91
Jonyx3d <dinisan2112@gmail.com>
2022-08-06 17:02:43 +0300

    finish first page

---
+++ src/choices.js
@@ -0,0 +1,19 @@
+// import Choices from "choices.js";
+// //import "choices.js/public/assets/styles/choices.min.css";
+
+// const select = document.querySelector(".select__item");
+
+// const choices = new Choices(select);
+
+// fetch("http://localhost:3000/countries")
+// 	.then((response) => response.json())
+// 	.then((countriesData) => {
+// 		const countries = countriesData.map(function (countryItem) {
+// 			return {
+// 				value: countryItem.id,
+// 				label: countryItem.name,
+// 			};
+// 		});
+
+// 		choices.setChoices(countries);
+// 	});
