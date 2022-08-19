e9cf4850b739fe7fd2c7e67f3433884a6601d41b
Jonyx3d <dinisan2112@gmail.com>
2022-08-19 21:36:09 +0300

    add modal and form progress

--- src/index.js
+++ src/index.js
@@ -54,24 +54,73 @@ selectInner.addEventListener("click", function (event) {
 		selectInner.classList.remove("select__inner--open");
 		selectTypeBtn.classList.remove("select__btn--open");
 		sendingType.value = buttonBox.value;
-		sendingType.dispatchEvent(new Event("change"));
+		sendingType.dispatchEvent(new Event("change", { bubbles: true }));
 	}
 });
 //==================================================
-function onChangeForm(event) {
-	const input = event.target;
-	console.log(input.value);
-}
+const tabsOptions = form.querySelector(".tabs__options");
+const computation = form.querySelector(".computation");
+const lengthInput = form.querySelector(`[name="length"]`);
+const widthInput = form.querySelector(`[name="width"]`);
+const heightInput = form.querySelector(`[name="height"]`);
+const weightInput = form.querySelector(`[name="weight"]`);
 form.addEventListener("change", function (event) {
-	onChangeForm(event);
+	if (
+		selectFrom.value !== "" &&
+		selectTo.value !== "" &&
+		sendingType.value !== ""
+	) {
+		tabsOptions.classList.add("tabs__options--open");
+	}
+	if (sendingType.value !== "parcel") {
+		weightInput.value = sendingType.value === "envelope-small" ? "0.3" : "0.5";
+		weightInput.disabled = true;
+	} else {
+		weightInput.disabled = false;
+	}
 });
-// form.addEventListener("input", function (event) {
-// 	onChangeForm(event);
-// });
+form.addEventListener("input", function (event) {
+	if (
+		lengthInput.value !== "0" &&
+		widthInput.value !== "0" &&
+		heightInput.value !== "0" &&
+		weightInput.value !== "0"
+	) {
+		computation.classList.add("computation--open");
+	}
+});
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
 	console.log(event);
 });
-sendingType.addEventListener("change", function (event) {
-	onChangeForm(event);
+//==========================================
+const discountButton = form.querySelector(".computation__discont-btn");
+const discountModal = document.querySelector("#discount-modal");
+const discountModalCloseBtn = document.querySelector(".modal__close");
+
+discountButton.addEventListener("click", function (event) {
+	openModal();
 });
+discountModalCloseBtn.addEventListener("click", function () {
+	closeModal();
+});
+discountModal.addEventListener("click", function (event) {
+	if (event.target.classList.contains("modal")) {
+		closeModal();
+	}
+});
+function closeModalOnEscape(event) {
+	if (event.code === "Escape") {
+		closeModal();
+	}
+}
+function openModal() {
+	document.addEventListener("keydown", closeModalOnEscape);
+	discountModal.classList.add("modal--open");
+	document.body.style.overflow = "hidden";
+}
+function closeModal() {
+	document.removeEventListener("keydown", closeModalOnEscape);
+	discountModal.classList.remove("modal--open");
+	document.body.style.overflow = "";
+}
